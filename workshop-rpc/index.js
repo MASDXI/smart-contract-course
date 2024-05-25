7e4602070f779d8da60ff89b2bc578088f3bd30c
MASDXI <sirawitt42@gmai.com>
2024-05-25 15:55:58 +0700

    update rpc call and hardhat for simulate blockchain

--- workshop-rpc/index.js
+++ workshop-rpc/index.js
@@ -1,63 +1,44 @@
-// Importing Web3.js and Ethers.js
-const Web3 = require('web3');
-const { ethers } = require('ethers');
+const { ethers, JsonRpcProvider, WebSocketProvider } = require('ethers');
+// const { Web3 } = require('web3');
 
-// Connecting to an Ethereum node using Web3.js
-const web3 = new Web3('https://mainnet.infura.io/v3/YOUR_INFURA_PROJECT_ID');
-web3.eth.getBlockNumber()
-    .then(blockNumber => {
-        console.log('Current Ethereum block number:', blockNumber);
-    })
-    .catch(error => {
-        console.error('Error connecting to Ethereum:', error);
+// HTTPS with Etherjs
+const ethershttp = async () => {
+  const provider = new JsonRpcProvider("http://localhost:8545/");
+  const network = await provider.getNetwork();
+  console.log("🚀 ~ ethershttp ~ network:", network.chainId)
+};
+
+ethershttp();
+
+// Websocket with Ethers
+const etherws = async (blocks) => {
+    const provider = new WebSocketProvider("ws://localhost:8545/");
+    let i = 1;
+    provider.on('block', async (blockNubmer) => {
+        let hash = await provider.getBlock(blockNubmer);
+        console.log("🚀 ~ provider.on ~ hash:", hash)
+        console.log('New Block: ' + blockNubmer);
+        if (i >= blocks) {
+            provider.off('block',);
+            process.exit()
+        }
+        i++;
     });
+};
 
-// Connecting to an Ethereum node using Ethers.js
-const provider = new ethers.providers.JsonRpcProvider('https://mainnet.infura.io/v3/YOUR_INFURA_PROJECT_ID');
-provider.getBlockNumber()
-    .then(blockNumber => {
-        console.log('Current Ethereum block number:', blockNumber);
-    })
-    .catch(error => {
-        console.error('Error connecting to Ethereum:', error);
-    });
+// etherws(15);
 
-// Comparison with connecting to databases
-// Connecting to PostgreSQL
-// const { Pool } = require('pg');
-// const pgPool = new Pool({
-//     user: 'your_username',
-//     host: 'localhost',
-//     database: 'your_database',
-//     password: 'your_password',
-//     port: 5432,
-// });
-// pgPool.query('SELECT NOW()', (err, res) => {
-//     if (err) {
-//         console.error('Error connecting to PostgreSQL:', err);
-//     } else {
-//         console.log('Connected to PostgreSQL. Current timestamp:', res.rows[0].now);
-//     }
-//     pgPool.end();
-// });
+// web3js library
+const { Web3 } = require('web3');
 
-// Connecting to MongoDB
-// const { MongoClient } = require('mongodb');
-// const uri = 'mongodb://localhost:27017';
-// const mongoClient = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
-// mongoClient.connect((err, client) => {
-//     if (err) {
-//         console.error('Error connecting to MongoDB:', err);
-//     } else {
-//         console.log('Connected to MongoDB.');
-//         const db = client.db('your_database');
-//         db.command({ serverStatus: 1 }, (err, res) => {
-//             if (err) {
-//                 console.error('Error checking MongoDB server status:', err);
-//             } else {
-//                 console.log('MongoDB server status:', res);
-//             }
-//             client.close();
-//         });
-//     }
-// });
+const web3 = new Web3('http://localhost:8545');
+
+const web3http = async () => {
+    let network = await web3.eth.getChainId();
+    console.log("🚀 ~ web3http:", network);
+};
+
+web3http();
+
+
+// For more methods: https://docs.web3js.org/libdocs/Web3Eth
